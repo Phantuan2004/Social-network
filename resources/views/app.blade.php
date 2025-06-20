0ff83cc47bb6c572365ccd3c83c4877a148aced7
Phantuan <Phanhoangtuan21072004@gmail.com>
2025-06-20 15:04:29 +0700

    change favicon web

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <!-- Favicon -->
-    <link href="public/images/favicon.png" rel="icon" type="image/png">
+    <link href="{{ asset('images/favicon.png') }}" rel="icon" type="image/png">
 
     <!-- title and description-->
     <title>Socialite</title>
