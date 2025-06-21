9142b77eac239cb0b44da75bf98ac1f99b2327ef
Phantuan <Phanhoangtuan21072004@gmail.com>
2025-06-22 00:50:08 +0700

    edit file model

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,6 +21,15 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'fullname',
+        'avatar',
+        'bio',
+        'gender',
+        'birthday',
+        'phone',
+        'address',
+        'role',
+        'is_active',
     ];
 
     /**
