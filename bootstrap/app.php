dd34c8ac2642337f564692f65fda6e5d21836ca2
Phantuan <Phanhoangtuan21072004@gmail.com>
2025-07-03 20:58:00 +0700

    config routing

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,15 +3,24 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+// use Laravel\Sanctum\Sanctum;
 
 return Application::configure(basePath: dirname(__DIR__))
+    ->withProviders([
+        // Sanctum::class,
+    ])
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        api: __DIR__ . '/../routes/api.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->alias([
+            'api' => \Illuminate\Routing\Middleware\ThrottleRequests::class . ':api',
+            'bindings' => \Illuminate\Routing\Middleware\SubstituteBindings::class,
+        ]);
+        $middleware->validateCsrfTokens(except: ['api/*']);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
